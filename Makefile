f80ce9fec69d24a265299dca945959a08d06d36d
generic-pers0n <58306640+generic-pers0n@users.noreply.github.com>
2019-11-28 12:14:25 -0800

    Add initial files to repository
    Added initial files to this repository

---
+++ Makefile
@@ -0,0 +1,6 @@
+lines: lines.cpp liblines.so
+	cd ..
+	g++ -o ../src/lines lines.cpp -L. -llines -g --std=c++17
+	mv lines ../bin
+	mv liblines.so ../bin
+
---
+++ lines.cpp
@@ -0,0 +1,21 @@
+/* Lines v2.0
+ * Avery King <avery98@pm.me> 
+ *
+ * This is where the core application is. It contains the basic and
+ * neccessary application functions. There are 4 options total:
+ *  - Find a graph's equation, slope, and y-intercept using two coordinate points
+ *  - Find a graph's equation and y-intercept using the slope and one coordinate point
+ *  - Generate coordinate points and optionally save them to a file
+ *  - Generate one coordinate point
+ **/
+// #include "libcalc.hpp"
+#include "lines.hpp"
+
+int main() {
+  // Clear the screen before the screen
+  clear();
+
+  // Display the main screen
+  main_screen("", "");
+}
+
