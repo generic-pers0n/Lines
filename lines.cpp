f23bf486fff77d58d7c75d2d186f36ef45663476
generic-pers0n <58306640+generic-pers0n@users.noreply.github.com>
2019-11-29 14:49:15 -0800

    Add copyright notices
    I've decided to license Lines under the GPL, version 3 OR later. More copyright notices will be added throughout the program.

--- lines.cpp
+++ lines.cpp
@@ -1,5 +1,18 @@
+/* Copyright (C) 2019 Avery King <avery98@pm.me>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 /* Lines v2.0
- * Avery King <avery98@pm.me> 
  *
  * This is where the core application is. It contains the basic and
  * neccessary application functions. There are 4 options total:
