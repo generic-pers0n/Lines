6b53bc7ba21ce85e526e9ddb709477f63b4ccd62
Avery King <avery98@pm.me>
2019-12-07 10:01:35 -0800

    Fix main_screen() declaration

--- lines.hpp
+++ lines.hpp
@@ -33,7 +33,7 @@ void generate_points(double slope, double y_intercept, int max_number, int start
 void generate_point(double slope, double y_intercept, int number);
 void write_generated_points_to_file(double slope, double y_intercept, int max_number, int starting_number, string file_name);
 
-void main_screen(string custom_data, string prompt_type, bool banner);
+void main_screen(string custom_data, string prompt_type);
 void options(string option); // For use later on
 void clear();
 void help();
