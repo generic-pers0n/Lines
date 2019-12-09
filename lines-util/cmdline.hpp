7777be2fb72ebe5f83eca68b03190524dcd2dae7
Avery King <avery98@pm.me>
2019-12-09 09:02:58 -0800

    Rename cmd_line.hpp to cmdline.hpp amd remove exit()
    I had to rename cmd_line.hpp to cmdline.hpp to match the #include in cmdline.cpp. I also removed exit() because it really did nothing but return a given number.

--- lines-util/cmdline.hpp
+++ lines-util/cmdline.hpp
@@ -5,10 +5,6 @@
 using namespace std;
 
 class CmdLineError {
-  int exit(int status) {
-    return status;
-  }
-
   void fatal_error(string err_msg) {
     cout << "The program has encountered a fatal error. It cannot continue";
 
