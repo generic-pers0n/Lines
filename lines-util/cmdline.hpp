281e97cf3dc00fbbb08d0b9626898b392d323d6a
Avery King <avery98@pm.me>
2019-12-09 17:22:34 -0800

    Add support for colored text

--- lines-util/cmdline.hpp
+++ lines-util/cmdline.hpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class CmdLineError {
   void fatal_error(string err_msg, int exit_status = -1) {
-    cout << "The program has encountered a fatal error. It cannot continue";
+    cout << "\x1B[41mThe program has encountered a fatal error. It cannot continue.\0[0m";
 
     if (err_msg == "") {
       exit(exit_status);
@@ -17,6 +17,10 @@ class CmdLineError {
 
     cout << "Error message: " << err_msg << endl;
     exit(exit_status);
+  0}
+
+  void warning(string err_msg) {
+    cout << "\x1B[30;43mWarning - " << err_msg << "\0[0m" << endl;
   }
 };
 
