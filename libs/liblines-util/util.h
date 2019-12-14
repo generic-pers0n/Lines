0756ae790e462150b66a3ceef43bfbaed78c5c4d
Avery King <avery98@pm.me>
2019-12-14 11:37:04 -0800

    Add prompt(), single_prompt(), and failsafe().
    Severall functions have now been implemented. The prompt() and
    single_prompt() are prompts that prompts the user. More information
    will be on a new wiki page.
    
    failsafe() is the failsafe mode function. This isn't finished yet, but
    will be finished sooner or later. More information will be on a new
    wiki page.

--- libs/liblines-util/util.h
+++ libs/liblines-util/util.h
@@ -1,5 +1,43 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#define FAILESAFE 0 // false
 using namespace std;
+
+namespace Util {
+  string prompt() {
+    string option;
+    cout << ">> ";
+    getline(cin, option);
+
+    return option;
+  }
+
+  string single_prompt() {
+    string option;
+    cout << ">> ";
+    cin >> option;
+
+    return option;
+  }
+
+  void failsafe() {
+    #undef FAILSAFE
+    #define FAILSAFE 1 // true
+
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+    cout << "!! Lines has entered failsafe mode       !!\n";
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n";
+
+    cout << "An error occured so serious that Lines cannot function normally. There\n";
+    cout << "are numerous reasons why this happens.\n\n";
+
+    cout << "In failsafe mode, normal functionality is disabled. You only have basic\n";
+    cout << "functionality. All plugins are unloaded unless there was an error doing\n";
+    cout << "so. Plugin functionality is also limited.\n\n";
+
+    cout << "For a list of commands, type \'help\'. Note that no normal command will be\n";
+    cout << "available during this time.\n\n";
+  }
+}
 
