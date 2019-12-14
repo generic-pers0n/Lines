ae3d849497e9903f8b07c2f09320d68501300f60
Avery King <avery98@pm.me>
2019-12-13 17:47:47 -0800

    Remove argument()

--- libs/lines-util/cmdline.cpp
+++ libs/lines-util/cmdline.cpp
@@ -1,17 +1,5 @@
 #include "cmdline.hpp"
 
-void action(int argc, string str_argv, string option_list) {
-  char *argv = str_argv.c_str();
-  int opt_val;
-
-  while ((opt_val = getopt(argc, argv, option_list.c_str())) != -1) {
-    switch (opt_val) {
-      case 'slope':
-        cout << "slope\n";
-    }
-  }
-}
-
 void prompt() {
   // Command values
   string option;
