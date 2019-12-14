476029de5e315ca83f24769510ba47a13f9fb82b
Avery King <avery98@pm.me>
2019-12-13 21:01:11 -0800

    Add load() to lpapi.cpp

--- api/lpapi/lpapi.cpp
+++ api/lpapi/lpapi.cpp
@@ -2,5 +2,15 @@
 #include <lines/util.h>
 
 if (FAILSAFE == 1) {
-  cout << "WARNING: You are in failsafe mode.\n";
+  cout << "WARNING: You are in failsafe mode. Plugin do not work in failsafe mode\n";
 }
+
+void load(string plugin) { // Load plugin
+  string plugin_path_full = "/lib64/lines-plugins/" + plugin;
+  void *plugin = dlopen(plugin_path_full.c_str(), RTLD_NOW);
+
+  if (!plugin) {
+    cout << "Failed to load plugin: " << plugin << endl;
+  }
+}
+
