9c9f97eec0e2f6816ae424ca168945d1694b4f38
Avery King <avery98@pm.me>
2019-12-17 18:22:06 -0800

    Add 'slope' command
    Previously, options were handled by Lines itself. Now, that's going
    to chagne. liblines-util is going to handle different commands.
    
    As part of an effort to phase out command handling by Lines, I've
    implemented a new command, "slope". This is the equivalent of option
    1, and has the same functionality too. More commands are expected to
    be added.

--- libs/liblines-util/util.cpp
+++ libs/liblines-util/util.cpp
@@ -1,3 +1,4 @@
+#include <boost/alogrithm/string/split.hpp>
 #include <lines/util.h>
 
 namespace Util {
@@ -10,7 +11,20 @@ namespace Util {
 
   void get_single_option(string option) {
     if (option == "slope") {
+      double xcoor1, ycoor1, xcoor2, ycoor2;
 
+      // Get the coordinate points from the user
+      cout << "Enter coordinate point 1: ";
+      cin >> xcoor1 >> ycoor1;
+
+      cout << "Enter coordinate point 2: ";
+      cin >> xcoor2 >> ycoor2;
+
+      // Calculate the equation and so with two coordinate points
+      coordinate(xcoor1, ycoor1, xcoor2, ycoor2);
+
+      // Return to the prompt (for single prompt)
+      single_prompt();
     }
 
     else {
