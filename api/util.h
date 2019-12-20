7aab2b333bfd0f738205f23665dfbf4a9409216e
Avery King <avery98@pm.me>
2019-12-20 15:42:25 -0800

    Implement failsafe mode
    After removing failsafe mode from util.cpp, failsafe mode has finally
    been implemented. Also, there have been some changes as well compared
    to the code that has been in util.cpp (before it was removed).

--- api/util.h
+++ api/util.h
@@ -6,8 +6,17 @@
 using namespace std;
 
 namespace Util {
-  // Function declarations
-  void failsafe(string prompt_type);
+  // Function declarations (internal only)
+  void get_failsafe_option_single(string option);
+  void get_failsafe_option(vector<string> option);
+
+  vector<string> prompt() {
+    vector<string> commands;
+    string input;
+
+    cout << ">> ";
+    getline(cin, input);
+
 
   vector<string> prompt() {
     vector<string> commands;
@@ -24,8 +33,32 @@ namespace Util {
 
     cout << ">> ";
     cin >> option;
-
     return option;
+  }
+
+  void failsafe(string prompt_type) {
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+    cout << "!! Lines has entered failsafe mode      !!\n";
+    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+
+    cout << "A problem occured and Lines has entered failsafe\n";
+    cout << "mode. Lines will have limited functionality in this\n";
+    cout << "mode.\n\n";
+
+    cout << "In this mode, normal functionality is disabled. You can\n";
+    cout << "try to continue into normal mode, but after 5 tries, that\n";
+    cout << "option will be disabled. For more information, please visit\n";
+    cout << "the Lines wiki.\n\n";
+
+    if (prompt_type == "single") {
+      string answer = single_prompt();
+      get_failsafe_option(answer);
+    }
+
+    else if (prompt_type == "default") {
+      vector<string> answer = prompt();
+      get_failsafe_option_single(answer);
+    }
   }
 }
 
