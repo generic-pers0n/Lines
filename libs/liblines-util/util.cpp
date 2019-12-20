4f4ded3c5021b60c3ccdb96ad409d44c44e929fd
Avery King <avery98@pm.me>
2019-12-20 13:33:17 -0800

    Move failsafe() to util.h

--- libs/liblines-util/util.cpp
+++ libs/liblines-util/util.cpp
@@ -36,25 +36,4 @@ namespace Util {
   void get_option(vector<string> option) {
 
   }
-
-  void failsafe() {
-    #undef FAILSAFE
-    #define FAILSAFE true
-
-    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
-    cout << "!! Lines has entered failsafe mode       !!\n";
-    cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n";
-
-    cout << "Failsafe mode is a special kind of mode that Lines enters when there\'s\'\n";
-    cout << "something wrong. Many things can cause Lines to go into failsafe mode.\n\n";
-
-    cout << "In failsafe mode, normal functionality is disabled. You only have basic\n";
-    cout << "functionality. All plugins are unloaded unless there was an error doing\n";
-    cout << "so. Plugin functionality is also limited.\n\n";
-
-    cout << "For a list of commands, type \'help\'. Note that no normal command will be\n";
-    cout << "available during this time.\n\n";
-
-    single_prompt();
-  }
 }
