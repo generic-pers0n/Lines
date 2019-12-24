3e4960362347ead4944584f6b0805e88f633121e
Avery King <avery98@pm.me>
2019-12-23 16:25:06 -0800

    Update util.cpp
    Many updates have been made to util.cpp. This includes the following:
      * Updates to the regular command line
      * Updates to the failsafe mode prompt
      * Added a copyright message
    
    There have been various changes to the regular command line. The first
    one being the addition of the "about" command. This features an updated
    about screen along with a copyright message while featuring other things
    as well.
    
    The failsafe prompt is now a multi-prompt. The failsafe single-prompt will
    be removed in upcomming changes. Other than that, only neccessary code
    changes were done to make detecting commands compatible with the multi-prompt.
    
    Before this commit, there was no copyright information in the source code. I've
    finally updated this so that there is copyright information.

--- libs/liblines-util/util.cpp
+++ libs/liblines-util/util.cpp
@@ -1,24 +1,43 @@
+/* Copyright (C) 2019 Avery King <avery98@pm.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <lines/liblines.h>
 #include <lines/util.h>
 
 namespace Util {
   ///////////////////// Get failsafe prompt option /////////////////////
   void get_failsafe_option(vector<string> option) {
-
-  }
-
-  void get_failsafe_option_single(string option) {
-    if (option == "continue") {
+    if (option[0] == "continue") {
       cout << "Attempting to continue...\n";
     }
 
-    else if (option == "exit") {
+    else if (option[0] == "exit") {
       cout << "Exiting...";
     }
 
     else {
-      cerr << option << " is not a known command\n";
+      cerr << "Unknown command\n";
+
+      // Return to the prompt
+      vector<string> input = prompt();
+      get_failsafe_option(input);
     }
+  }
+
+  void get_failsafe_option_single(string option) {
+
   }
   //////////////////////////////////////////////////////////////////////
 
@@ -37,7 +56,23 @@ namespace Util {
       coordinate(xcoor1, ycoor1, xcoor2, ycoor2);
 
       // Return to the prompt (for single prompt)
-      single_prompt();
+      string option = single_prompt();
+      get_single_option(option);
+    }
+
+    else if (option == "about") {
+      cout << "(C) 2019 Avery King\n";
+      cout << "Lines, the Swiss Army knife for Linear Graphs\n";
+      cout << "Partially made during a Xoads live stream\n";
+      cout << "We miss you, Xoads!\n\n";
+      cout << "Lines  Copyright (C) 2019  Avery King\n";
+      cout << "This program comes with ABSOLUTELY NO WARRANTY; for details type `copyright'.\n";
+      cout << "This is free software, and you are welcome to redistribute it\n";
+      cout << "under certain conditions; type `copyright' for details.\n\n";
+
+      // Return to the prompt (for single prompt)
+      string option = single_prompt();
+      get_single_option(option);
     }
 
     else {
