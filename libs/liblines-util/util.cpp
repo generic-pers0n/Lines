90e0dd74964f5d72db9929746d53d3392cf8d2b3
Avery King <avery98@pm.me>
2019-12-26 16:01:57 -0800

    Add "gencoor" command and fix about command
    I've added a new command called "gencoor". This command is meant
    to replace options 2, 3, and 4. However, currently, gencoor has
    options 2 and 3 implemented. Option 4 will soon be implemented.
    
    gencoor (for now) has several options:
      * No option (default)
      * "write"
    
    The default option (no option) represents option 2 while "write"
    represents option 3. It is planned that there will be a "single"
    option (representing option 4).
    
    For the about command, previously, it did not return you to the
    prompt like it was supposed to. This has since been fixed to return
    you to main prompt.

--- libs/liblines-util/util.cpp
+++ libs/liblines-util/util.cpp
@@ -38,7 +38,7 @@ namespace Util {
   //////////////////////////////////////////////////////////////////////
 
   void get_option(vector<string> option) {
-    if (option[0] == "slope") { // Slope command
+    if (option[0] == "slope") { // "slope" command
       double xcoor1, ycoor1, xcoor2, ycoor2;
 
       // Get the coordinate points from the user
@@ -56,7 +56,89 @@ namespace Util {
       get_option(option);
     }
 
-    else if (option[0] == "about") { // About command
+    else if (option[0] == "coordinate") { // "coordinate" command
+      // Variables
+      double xcoor, ycoor, slope_value;
+
+      // Get the slope from the user
+      cout << "Enter the slope of the graph: ";
+      cin >> slope_value;
+
+      // Get the coordinate points from the user
+      cout << "Enter coordinate point x: ";
+      cin >> xcoor;
+
+      cout << "Enter coordinate point y: ";
+      cin >> ycoor;
+
+      // Calculate the equation
+      slope(xcoor, ycoor, slope_value);
+
+      // Return to the prompt
+      vector<string> option = prompt();
+      get_option(option);
+    }
+
+    else if (option[0] == "gencoor") { // "gencoor" command
+      // Variables
+      double y_intercept;
+      double slope;
+      int max_number;
+      int starting_number;
+
+      if (option[1] == "") { // default option
+        // Get the y-intercept and slope
+        cout << "What is the slope? ";
+        cin >> slope;
+
+        cout << "What is the y-intercept? ";
+        cin >> y_intercept;
+
+        cout << "What number do you want to start at? ";
+        cin >> starting_number;
+
+        // Get the amount of coordinate points the user wants
+        cout << "What number should be the maximum generated (no decimals)? ";
+        cin >> max_number;
+
+        generate_points(slope, y_intercept, max_number, starting_number);
+
+        // Return to the prompt
+        vector<string> option = prompt();
+        get_option(option);
+      }
+
+      else if (option[1] == "write") {
+        // Get the and slope
+        cout << "What is the slope and y-intercept";
+        cin >> slope >> y_intercept;
+
+        // Get the number the user wants to start at
+        cout << "What number do you want to start at? ";
+        cin >> starting_number;
+
+        // Get the amount of coordinate points the user wants
+        cout << "What number should be the maximum generated (no decimals)? ";
+        cin >> max_number;
+
+        cout << "The file will be named Coordinate Points.txt\n";
+        cout << "It will be saved to your desktop\n\n";
+
+        #ifdef __unix__
+          write_generated_points_to_file(slope, y_intercept, max_number, starting_number, "Coordinate Points.txt");
+          system("mv \"Coordinate Points.txt\" ~/Desktop");
+        #elif _WIN32
+          write_generated_points_to_file(slope, y_intercept, max_number, starting_number, "Coordinate Points.txt");
+          system("move \"Coordinate Points.txt\" %userprofile%\\Desktop");
+        #endif
+
+        // Return to the main prompt
+        vector<string> option = prompt();
+        get_option(option);
+      }
+    }
+
+    else if (option[0] == "about") { // "about" command
       cout << "====================================\n";
       cout << "Lines v3.0 dev\n";
       cout << "====================================\n\n";
@@ -74,7 +156,21 @@ namespace Util {
       cout << "This is free software, and you are welcome to redistribute it\n";
       cout << "under certain conditions; type `copyright' for details.\n";
 
-      // Return to the prompt (for single prompt)
+      // Return to the prompt
+      vector<string> option = prompt();
+      get_option(option);
+    }
+
+    else if (option[0] == "exit") { // "exit" command
+      // Stuff to clean up goes here.
+      //
+      // Mainly, there should be an unload function here. That
+      // function would unload any plugins, close things, etc.
+      cout << "Bye!";
+      exit(0);
+    }
+
+    else if (option[0] == "") { // If nothing was entered
       vector<string> option = prompt();
       get_option(option);
     }
