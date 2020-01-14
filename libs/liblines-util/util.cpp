d07a76c477d1c28e9b8ae48f6e0e85001cad3e33
Avery King <avery98@pm.me>
2020-01-14 15:09:06 -0800

    Change #includes to local #includes
    Instead of having to install the headers yourself before compiling,
    I've changed this to use local headers found in various project
    directories.

--- libs/liblines-util/util.cpp
+++ libs/liblines-util/util.cpp
@@ -13,8 +13,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include <lines/liblines.h>
-#include <lines/util.h>
+#include "../liblines/liblines.h"
+#include "util.h"
 
 namespace Util {
   ///////////////////// Get failsafe prompt option /////////////////////
